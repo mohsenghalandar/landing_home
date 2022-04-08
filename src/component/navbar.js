879d56d00d252fea8482b24519ed47256d6af42b
mohsen ghalandar <mohsenghalandar1998@gmail.com>
2022-04-08 17:48:13 +0430

    change project from basic and start it again

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,20 +1,24 @@
 import React , {Component} from 'react';
 import style from "./navbar.module.css";
 import logo from "../resources/image/mamad-power.jpg";
+import { BiUserCircle } from 'react-icons/bi';
+
 
 const Navbar = () => {
     return (
         <div className={style.header}>
+            <h2 className={style.symbol} >نام شرکت</h2>
             <div className={style.nav}>
                 <ul className={style.list}>
                     <li className={style.item}>صفحه اصلی</li>
-                    <li className={style.item}>نمونه کارها</li>
-                    <li className={style.item}> رزومه</li>
+                    <li className={style.item}>مقالات</li>
+                    <li className={style.item}> درباره ما</li>
                     <li className={style.item}> ارتباط با ما</li>
                 </ul>
             </div>
             <div className={style.logocon} >
-                <img className={style.logo} src={logo} alt="our band name" title="Hi,there 😍👌" />
+                <BiUserCircle className={style.logo} src={logo} alt="our band name" title="Hi,there 😍👌">ورود</BiUserCircle>
+                <p className={style.login}>ورود</p>
             </div>
         </div>
     );
